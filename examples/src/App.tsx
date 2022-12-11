0c200ef1d0959fddb7ea1b78d8108def6fa4686e
Alexander Priteev <priteev@sibers.com>
2022-12-11 20:13:41 +0600

    init: init presentation

---
+++ examples/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.scss';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <HashRouter>
+      <Routes></Routes>
+    </HashRouter>
+  );
+}
+
+export default App;
